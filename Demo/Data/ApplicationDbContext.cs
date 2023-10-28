d01eb18e971b875542597d5179b915dec68d9e90
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2023-10-28 17:41:21 +0545

    Add project files.

---
+++ Demo/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Demo.Areas.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
